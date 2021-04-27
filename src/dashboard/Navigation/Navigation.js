d4829f9b1053eff80d4992cd892a507051d7e561
Ben Lapidus <blapidus@u.rochester.edu>
2021-04-27 01:31:19 -0400

    Updated router

--- src/dashboard/Navigation/Navigation.js
+++ src/dashboard/Navigation/Navigation.js
@@ -222,8 +222,8 @@ export default function Navigation() {
             {/* Recent Deposits */}
             <Grid lg={12}>
               <Route exact path="/park-viz" component={Workflow} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/support" component={Support} />
+              <Route exact path="/park-viz/about" component={About} />
+              <Route exact path="/park-viz/support" component={Support} />
               <CSVReader2 passUserData={setUserData}></CSVReader2>
               {userData[0][0]}
             </Grid>
--- src/dashboard/Navigation/listItems.js
+++ src/dashboard/Navigation/listItems.js
@@ -6,7 +6,6 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import InfoIcon from '@material-ui/icons/Info';
 import HelpIcon from '@material-ui/icons/Help';
-import SettingsIcon from '@material-ui/icons/Settings'
 import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
@@ -16,30 +15,24 @@ import { Router, Route, Link } from "react-router-dom";
 export const mainListItems = (
   <div>
 
-    <ListItem button component={Link} to="/park-viz">
+    <ListItem button component={Link} to="/park-viz/">
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItem>
 
-    <ListItem button component={Link} to="/about">
+    <ListItem button component={Link} to="/park-viz/about">
       <ListItemIcon>
         <InfoIcon />
       </ListItemIcon>
       <ListItemText primary="About PARK test" />
     </ListItem>
-    <ListItem button component={Link} to="/support">
+    <ListItem button component={Link} to="/park-viz/support">
       <ListItemIcon>
         <HelpIcon />
       </ListItemIcon>
       <ListItemText primary="Support" />
-    </ListItem>
-    <ListItem button component={Link} to="/settings">
-      <ListItemIcon>
-        <SettingsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Settings" />
     </ListItem>
   </div>
 );
